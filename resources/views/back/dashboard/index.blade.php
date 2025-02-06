1500d325de3bf5311f660eafe0ab29031dbd11be
HADID <hadidsurya123@gmail.com>
2025-02-06 12:03:14 +0700

    Menyelesaikan konflik setelah pull

--- resources/views/back/dashboard/index.blade.php
+++ resources/views/back/dashboard/index.blade.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -517,3 +519,4 @@
 <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
 <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
 <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</html>
