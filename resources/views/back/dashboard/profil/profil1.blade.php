b6bc0e7cd48d8dc36b30f8221c9c3cc4f087b193
HADID <hadidsurya123@gmail.com>
2025-02-06 08:25:36 +0700

    Menyelesaikan konflik setelah pull

--- resources/views/back/dashboard/profil/profil1.blade.php
+++ resources/views/back/dashboard/profil/profil1.blade.php
@@ -305,6 +305,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Core theme JS-->
 <script src="{{ asset('back/js/scripts.js') }}"></script>
+<script src="{{ asset('back/js/navbar.js') }}"></script>
 <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
 <!-- * *                               SB Forms JS                               * *-->
 <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
