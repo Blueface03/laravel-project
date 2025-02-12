45fff23b2e11aa399dc1e5e1756e98bcb140c89d
HADID <hadidsurya123@gmail.com>
2025-02-12 15:11:09 +0700

    Menyelesaikan konflik setelah pull

--- resources/views/back/layout/dokumen.blade.php
+++ resources/views/back/layout/dokumen.blade.php
@@ -8,7 +8,7 @@
             @break
 
             @default
-            Dokumen Renstra 2023-2027
+                Dokumen Renstra 2023-2027
         @endswitch
     </h3>
     <div class="divider2"></div>
@@ -23,9 +23,10 @@
             <h4>Renstra 2017-2022 Perubahan</h4>
             <p>Renstra 2017-2022 Perubahan</p>
             <div class="button-group2">
-                <a href="#" class="btn2"><i class="fas fa-download"></i> Download</a>
-                <a href="#" class="btn2"><i class="fas fa-book-open"></i> Lihat</a>
-                <a href="#" class="btn2"><i class="fas fa-link"></i> Copy Link</a>
+                <a href="#" class="btn-custom btn-custom-download-1"><i class="fas fa-download"></i> Download</a>
+                <a href="#" class="btn-custom btn-custom-read-2"><i class="fas fa-book-open"></i> Lihat</a>
+                <a href="#" class="btn-custom btn-custom-link-3"><i class="fas fa-link"></i> Copy Link</a>
+
             </div>
         </div>
 
@@ -33,9 +34,10 @@
             <h4>IKU Dinas Kesehatan DIY Tahun 2018-2022</h4>
             <p>IKU Dinas Kesehatan DIY Tahun 2018-2022</p>
             <div class="button-group2">
-                <a href="#" class="btn2"><i class="fas fa-download"></i> Download</a>
-                <a href="#" class="btn2"><i class="fas fa-book-open"></i> Lihat</a>
-                <a href="#" class="btn2"><i class="fas fa-link"></i> Copy Link</a>
+                <a href="#" class="btn-custom btn-custom-download-1"><i class="fas fa-download"></i> Download</a>
+                <a href="#" class="btn-custom btn-custom-read-2"><i class="fas fa-book-open"></i> Lihat</a>
+                <a href="#" class="btn-custom btn-custom-link-3"><i class="fas fa-link"></i> Copy Link</a>
+
             </div>
         </div>
 
@@ -43,9 +45,10 @@
             <h4>IKU Dinas Kesehatan DIY Tahun 2018-2022 Perubahan</h4>
             <p>IKU Dinas Kesehatan DIY Tahun 2018-2022 Perubahan</p>
             <div class="button-group2">
-                <a href="#" class="btn2"><i class="fas fa-download"></i> Download</a>
-                <a href="#" class="btn2"><i class="fas fa-book-open"></i> Lihat</a>
-                <a href="#" class="btn2"><i class="fas fa-link"></i> Copy Link</a>
+                <a href="#" class="btn-custom btn-custom-download-1"><i class="fas fa-download"></i> Download</a>
+                <a href="#" class="btn-custom btn-custom-read-2"><i class="fas fa-book-open"></i> Lihat</a>
+                <a href="#" class="btn-custom btn-custom-link-3"><i class="fas fa-link"></i> Copy Link</a>
+
             </div>
         </div>
     </section>
