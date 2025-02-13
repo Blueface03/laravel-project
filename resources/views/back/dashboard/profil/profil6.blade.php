9aa916fb29a63c6e82f7902ea4701792dda76dce
HADID <hadidsurya123@gmail.com>
2025-02-13 08:10:46 +0700

    Menyelesaikan konflik setelah pull

--- resources/views/back/dashboard/profil/profil6.blade.php
+++ resources/views/back/dashboard/profil/profil6.blade.php
@@ -1,64 +1,49 @@
 @include('back.layout.stylesheet')
 @include('back.layout.navbar')
-<!-- Header - set the background image for the header in the line below-->
-<header class="py-5 bg-image-full" style="background-image: url('https://source.unsplash.com/wfh8dDlNFOk/1600x900')">
-    <div class="text-center my-5">
-        <h1 class="text-white fs-3 fw-bolder">Full Width Pics</h1>
-        <p class="text-white-50 mb-0">Landing Page Template</p>
-        <h2>LHKPN</h2>
-    </div>
-</header>
-<!-- Content section-->
-<section class="py-5">
-    <div class="container my-5">
-        <div class="row justify-content-center">
-            <div class="col-lg-6">
-                <h2>Full Width Backgrounds</h2>
-                <p class="lead">A single, lightweight helper class allows you to add engaging, full width background
-                    images to sections of your page.</p>
-                <p class="mb-0">The universe is almost 14 billion years old, and, wow! Life had no problem starting
-                    here on Earth! I think it would be inexcusably egocentric of us to suggest that we're alone in the
-                    universe.</p>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- Content section-->
-<section class="py-5">
-    <div class="container my-5">
-        <div class="row justify-content-center">
-            <div class="col-lg-6">
-                <h2>Engaging Background Images</h2>
-                <p class="lead">The background images used in this template are sourced from Unsplash and are open
-                    source and free to use.</p>
-                <p class="mb-0">I can't tell you how many people say they were turned off from science because of a
-                    science teacher that completely sucked out all the inspiration and enthusiasm they had for the
-                    course.</p>
-            </div>
-        </div>
-    </div>
-</section>
 
-<!-- Footer-->
-<footer class="footer py-4">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-4 text-lg-start">Copyright &copy; DINAS DUKCAPIL KOTA TEGAL</div>
-            <div class="col-lg-4 my-3 my-lg-0">
-                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i
-                        class="fab fa-twitter"></i></a>
-                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i
-                        class="fab fa-facebook-f"></i></a>
-                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i
-                        class="fab fa-linkedin-in"></i></a>
-            </div>
-            <div class="col-lg-4 text-lg-end">
-                <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
-                <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
-            </div>
-        </div>
+<div class="unique-card-container">
+    <div class="unique-card card-1">
+      <h3>drg. Pembajun Setyaningastutie, M.Kes</h3>
+      <p>Kepala Dinas Kesehatan</p>
+      <a href="#" class="btn">Lihat LHKPN</a>
     </div>
-</footer>
+    <div class="unique-card card-2">
+      <h3>Dra. Siti Badriyah, Apt., M.Kes.</h3>
+      <p>Sekretaris</p>
+      <a href="#" class="btn">Lihat LHKPN</a>
+    </div>
+    <div class="unique-card card-3">
+      <h3>dr. Gregorius Anung Trihadi, M.P.H.</h3>
+      <p>Kepala Bidang Pelayanan Kesehatan</p>
+      <a href="#" class="btn">Lihat LHKPN</a>
+    </div>
+    <div class="unique-card card-4">
+      <h3>M. Agus Priyanto, S.K.M., M.Kes.</h3>
+      <p>Kepala Bidang Sumber Daya Kesehatan</p>
+      <a href="#" class="btn">Lihat LHKPN</a>
+    </div>
+    <div class="unique-card card-5">
+      <h3>Endang Pamungkassii, S.K.M., M.Kes.</h3>
+      <p>Kepala Bidang Kesehatan Masyarakat</p>
+      <a href="#" class="btn">Lihat LHKPN</a>
+    </div>
+    <div class="unique-card card-6">
+      <h3>Setiyo Harini, S.K.M., M.Kes.</h3>
+      <p>Kepala Bidang Pengendalian Penyakit</p>
+      <a href="#" class="btn">Lihat LHKPN</a>
+    </div>
+    <div class="unique-card card-7">
+      <h3>Suseno, S.Gz., M.P.H.</h3>
+      <p>Kepala Sub Bagian Umum</p>
+      <a href="#" class="btn">Lihat LHKPN</a>
+    </div>
+    <div class="unique-card card-8">
+      <h3>Siti Nurhayah Isfandiari, S.K.M., M.P.H.</h3>
+      <p>Kepala Seksi Promosi Kesehatan dan Tata Kelola Kesehatan Masyarakat</p>
+      <a href="#" class="btn">Lihat LHKPN</a>
+    </div>
+  </div>
+  
 
 @include('back.layout.footer')
 @include('back.layout.scripts')
